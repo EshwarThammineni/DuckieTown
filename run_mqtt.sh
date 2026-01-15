f9f51c190307182dd85b85e0e6f0abc1bb3b1ef4
Eshwar Thammineni <76793416+EshwarThammineni@users.noreply.github.com>
2026-01-15 15:52:28 -0500

    Simplify

--- run_mqtt.sh
+++ run_mqtt.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
+
+# Source ROS setup
 source /opt/ros/melodic/setup.bash
-export PYTHONPATH=/opt/ros/melodic/lib/python3/dist-packages:$PYTHONPATH
-
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_PACKAGE_PATH=/opt/ros/melodic/share
-
-exec python3 /root/scout-files/mqtt_client.py
+python3 /root/scout-files/mqtt_client.py
